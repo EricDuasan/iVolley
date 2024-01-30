9b5e7d076fe7bda11700263bd8db22f3df5ed9c5
Eric Duart <eric.duasan@gmail.com>
2024-01-30 21:38:14 +0100

    Update colors, SDK versions, and add LoginActivity and ShowLoginFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "es.ericd.ivolley"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "es.ericd.ivolley"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
