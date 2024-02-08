34db2828aa29fc50deb01dabe404402609375100
Eric Duart San Ignacio <eric.duasan@gmail.com>
2024-02-08 13:06:53 +0100

    Added timestamps to firestore chat collection

--- app/src/main/java/es/ericd/ivolley/services/FirestoreService.kt
+++ app/src/main/java/es/ericd/ivolley/services/FirestoreService.kt
@@ -2,10 +2,14 @@ package es.ericd.ivolley.services
 
 import android.util.Log
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import es.ericd.ivolley.dataclases.ChatUIDItem
 import es.ericd.ivolley.dataclases.Chatitem
 import kotlinx.coroutines.tasks.await
+import java.util.Calendar
+import java.util.GregorianCalendar
+import java.util.TimeZone
 
 class FirestoreService {
     companion object {
@@ -47,11 +51,23 @@ class FirestoreService {
         suspend fun getChats(): MutableList<Chatitem> {
             val firebaseDB = getInstance()
 
-            val chatsFromDB = firebaseDB.collection("chats").get().await()
+            // val chatsFromDB = firebaseDB.collection("chats").get().await()
+
+            val start = GregorianCalendar(TimeZone.getDefault())
+            start.add(Calendar.DAY_OF_MONTH, -2)
+
+            val end = GregorianCalendar(TimeZone.getDefault())
+            end.add(Calendar.DAY_OF_MONTH, 1)
+
+            // val timestamp = FieldValue.serverTimestamp()
+
+            // Get chats between a timestamp
+            val chatsFromDB = firebaseDB.collection("chats")
+                .whereGreaterThan("timestamp", start.time)
+                .whereLessThan("timestamp", end.time)
+                .get().await()
 
             val ids = getUsersIDs()
-
-            Log.d("data","CHATS")
 
             val chats = mutableListOf<Chatitem>()
 
@@ -83,9 +99,13 @@ class FirestoreService {
         suspend fun addChat(userUID: String, message: String) {
             val firebaseDB = getInstance()
 
+            // Get UTC timestamp from firebase
+            val timestamp = FieldValue.serverTimestamp()
+
             firebaseDB.collection("chats").add(mapOf(
                     "userUID" to userUID,
-                    "message" to message
+                    "message" to message,
+                    "timestamp" to timestamp
                 )
             ).await()
 
